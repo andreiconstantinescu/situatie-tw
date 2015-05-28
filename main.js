8d1a642ff0ea0770b55e2f6040ade40e19342b37
Andrei Constantinescu <andrei@constantinescu.io>
2015-05-28 12:09:58 +0300

    Update partial grades.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var students = [
   {
     name: "ACHIAUA G ANDREI-MARIAN",
-    att: 6,
+    att: 7,
     p1: 10,
-    test: 10
+    test: 10,
+    p2: 10
   },
   {
     name: "ANITA M LIVIU-BRANCO",
@@ -18,40 +19,40 @@ var students = [
     test: 10,
   },
   {
-    name: "BUNGET M ALEXANDRU-CATALIN",
-    att: 0,
-    p1: 0,
-    test: 0
-  },
-  {
     name: "CIOBOTARU C MIHAELA-GABRIELA",
-    att: 7,
+    att: 8,
     test: 10,
     p1: 10,
+    p2: 10
   },
   {
     name: "CIUBUC G FLORIN-MARIAN",
-    test: 0,
+    test: 3,
     p1: 10,
-    att: 3
+    att: 4,
+    p2: 10,
+    p3: 20
   },
   {
     name: "CRETU M SILVIU-CONSTANTIN",
     test: 10,
     p1: 10,
-    att: 3
+    att: 4,
+    p2: 10
   },
   {
     name: "DINU F ALEXANDRU",
-    att: 8,
+    att: 9,
     test: 10,
-    p1: 10
+    p1: 10,
+    p2: 10
   },
   {
     name: "DRAGAN G VLAD-MARIUS",
     test: 10,
     p1: 10,
-    att: 3,
+    att: 4,
+    p3: 15,
   },
   {
     name: "DUMITRU V.M EMANUEL-CRISTIAN",
@@ -62,21 +63,24 @@ var students = [
   },
   {
     name: "GRAUR S PETRU-COSTIN",
-    att: 4,
+    att: 6,
     test: 8,
-    p1: 10
+    p1: 10,
+    p2: 10,
+    p3: 20
   },
   {
     name: "ION GH LAURENTIU-CRISTIAN",
-    att: 0,
+    att: 2,
     test: 0,
-    p1: 0
+    p1: 10,
+    p3: 15
   },
   {
     name: "JINGA C COSMIN",
     att: 9,
     test: 10,
-    p1: 'sa trimita'
+    p1: 10
   },
   {
     name: "LAZAR N CATALIN-ALEXANDRU",
@@ -86,21 +90,24 @@ var students = [
   },
   {
     name: "MELEANCA P IONUT-ADRIAN",
-    p1: 0,
+    p1: 10,
     test: 0,
-    att: 1
+    att: 3,
+    p2: 15
   },
   {
     name: "MIU I OVIDIU",
-    att: 3,
+    att: 4,
     test: 8,
-    p1: 9
+    p1: 9,
+    p2: 15
   },
   {
     name: "NABOULSI F MAYA",
-    att: 10,
+    att: 11,
     test: 10,
     p1: 10,
+    p2: 10
   },
   {
     name: "NGUYEN S VAN TU",
@@ -111,9 +118,10 @@ var students = [
   },
   {
     name: "NICULAI D DRAGOS-ANDREI",
-    att: 2,
+    att: 3,
     test: 10,
-    p1: 10
+    p1: 10,
+    p2: 10
   },
   {
     name: "NITA V MIHAITA-ALIN",
@@ -123,51 +131,61 @@ var students = [
   },
   {
     name: "PAUCEANU C.F ALEXANDRU",
-    att: 0,
+    att: 2,
     p1: 10,
-    test: 0
+    test: 0,
+    p3: 13
   },
   {
     name: "PETCULESCU M.C GEORGE-MIHAI",
-    att: 8,
+    att: 9,
     p1: 10,
-    test: 10
+    test: 10,
+    p2: 10
   },
   {
     name: "POPA I VLADUT-IONUT",
-    att: 2,
+    att: 4,
     p1: 10,
-    test: 5
+    test: 5,
+    p2: 10,
+    p3: 13
   },
   {
     name:"PREDESCU I MARA-CALINA",
     test: 10,
-    att: 10,
-    p1: 10
+    att: 11,
+    p1: 10,
+    p2: 10
   },
   {
     name: "PURDILA I ANDREI",
     test: 9,
     p1: 10,
-    att: 10
+    att: 11,
+    p2: 10
   },
   {
     name: "RAILEANU A.C LAURA-NICOLETA",
-    att: 6,
+    att: 8,
     test: 8,
-    p1: 10
+    p1: 10,
+    p2: 10
   },
   {
     name: "SAMSON I STEFAN",
     test: 10,
     p1: 10,
-    att: 2
+    att: 3,
+    p3: 15
   },
   {
     name: "SAVULESCU Z ROBERTO-MARIAN",
     att: 2,
     test: 0,
-    p1: 10
+    p1: 10,
+    p3: 15,
+    p2: 2
   },
   {
     name: "SIMUT V PAVEL",
@@ -177,9 +195,10 @@ var students = [
   },
   {
     name: "TATOMIR V RADU",
-    att: 2,
+    att: 3,
     test: 10,
-    p1: 10
+    p1: 10,
+    p2: 10
   },
   {
     name: "URSU GH ROBERT-ANDREI",
@@ -209,34 +228,34 @@ $(document).ready(function () {
   };
 
   var table = '<thead>';
-  var tableHeader = ['Nr', 'Nume', 'Prezente', 'Proiectul 1', 'Proiectul 2', 'Joc', 'Test', 'Punctaj', 'Nota', 'Nota Finala'];
+  var tableHeader = ['Nr', 'Nume', 'Prezente', 'Proiectul 1', 'Proiectul 2', 'Test', 'Activitate/Joc', 'Punctaj', 'Nota', 'Nota Finala'];
   for (var i = 0; i < tableHeader.length; i++) {
     table += '<td><strong>' + tableHeader[i] + '</strong></td>';
   }
   table += '</thead><tbody>';
   for (i = 0; i < students.length; i++) {
     var color;
-    if (students[i].att >= 8)
-      color = 'success';
-    else if (students[i].att >= 6)
-      color = 'info';
-      else
-        color = '';
-
-    if (students[i].att <= 2 && students[i].test < 5)
-      color = 'danger';
 
 
     var p1 = students[i].p1 ? students[i].p1 : 0;
     var p2 = students[i].p2 ? students[i].p2 : 0;
     var p3 = students[i].p3 ? students[i].p3 : 0;
     var test = students[i].test ? students[i].test : 0;
-    var prezenta = (students[i].att >= 8 ? 20 : students[i].att);
-    var punctaj = (typeof p1 === 'number' ? p1 : 0 ) + (typeof p2 === 'number' ? p2 : 0 ) + (typeof p3 === 'number' ? p3 : 0 ) + test + prezenta;
+
+    var prezenta = (students[i].att >= 8 ? 20 : (p3 ? p3 : students[i].att));
+    console.log(students[i].name, students[i].att, prezenta, prezenta, p3);
+    var punctaj = (typeof p1 === 'number' ? p1 : 0 ) + (typeof p2 === 'number' ? p2 : 0 ) + test + prezenta;
 
     var nota = getNota(punctaj);
     var notaFinala = Math.round(nota);
     notaFinala = notaFinala > 0 ? notaFinala : 1;
+
+    if (notaFinala < 5)
+      color = 'danger';
+    else if (notaFinala === 10)
+      color = 'success';
+      else
+        color = ''
 
     var trStart = '<tr class=' + color + '>';
     var tdNrStart = '<td>' + (i+1);
@@ -245,14 +264,15 @@ $(document).ready(function () {
     var tdAtt = '<td>' + students[i].att + '</td>';
     var tdP1 = '<td>' + p1 + '</td>';
     var tdP2 = '<td>' + p2 + '</td>';
-    var tdP3 = '<td>' + p3 + '</td>';
+    var tdActivitate = '<td>' + prezenta + '</td>';
     var tdTest = '<td>' + students[i].test + '</td>';
     var tdPunctaj = '<td>' + punctaj + ' puncte</td>';
     var tdNota = '<td>' + nota + '</td>';
     var tdNotaFinala = '<td><strong>' + notaFinala + '</strong></td>';
     var trEnd = '</tr>';
 
-    table += trStart + tdNrStart + tdNrEnd + tdName + tdAtt + tdP1 + tdP2 + tdP3 + tdTest + tdPunctaj + tdNota + tdNotaFinala + trEnd ;
+
+    table += trStart + tdNrStart + tdNrEnd + tdName + tdAtt + tdP1 + tdP2 + tdTest + tdActivitate + tdPunctaj + tdNota + tdNotaFinala + trEnd ;
   }
   table += '</tbody>';
   $('#tabel-note').html(table);
